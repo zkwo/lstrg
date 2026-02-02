5de6c1c39bbb531450b2eb87c6ba1b33dbbe9048
zkwo <ko40c980ew632@gmail.com>
2026-02-02 21:08:30 +0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,12 @@
+local games = {
+    [9872472334] = "https://api.jnkie.com/api/v1/luascripts/public/7e2065f68778b7cfa6630d267b3bc1f46dea67b798f1b2754cbe9ba295c5621b/download"))()",
+}
+
+local currentID = game.PlaceId
+local scriptURL = games[currentID]
+
+if scriptURL then
+    loadstring(game:HttpGet(scriptURL))()
+else
+    game.Players.LocalPlayer:Kick("Yo! This game ain't on the list.\nOnly Evade!")
+end
